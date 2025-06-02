16e5226269eb3148a75aaeee7006e35e7195e6e3
Rifky <andrarifky26@gmail.com>
2025-06-02 19:48:48 +0700

    hashrouter update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // udah diganti dari BrowserRouter
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/nihonme">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
